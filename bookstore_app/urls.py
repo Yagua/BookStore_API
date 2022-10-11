cb66940763a6c66ff4a4a5be4e306ecc1347bb1e
Yagua <yagua.db@gmail.com>
2022-10-10 23:41:02 -0500

    add template and view for restore password

--- bookstore_app/urls.py
+++ bookstore_app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from user.views import RestartUserPasswordView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,4 +13,9 @@ urlpatterns = [
     path("api/v1/carts/", include("cart.urls")),
     path("api/v1/profile/", include("user_profile.urls")),
     path("api/v1/search/", include("search.urls")),
+    path(
+        "password/reset/confirm/<str:uid>/<str:token>/",
+        RestartUserPasswordView.as_view(),
+        name="reset_password",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -1,2 +1,45 @@
+from django.views import View
+from django.shortcuts import render
+import json
+import requests
 
+class RestartUserPasswordView(View):
 
+    def get(self, request, uid, token):
+        return render(request, "reset_password.html")
+
+    def post(self, request, uid, token):
+        new_password = request.POST.get("new_password", None)
+        re_new_password = request.POST.get("re_new_password", None)
+
+        def prepare_errors(data):
+            errors = []
+            for error, value in data.items():
+                errors.append(
+                    f"[{error}]: {' - '.join(value)}"
+                )
+            return " || ".join(errors)
+
+        payload = {
+            "uid": uid,
+            "token": token,
+            "new_password": new_password,
+            "re_new_password": re_new_password
+        }
+
+        protocol = "http"
+        # host = request.get_host()
+        host = "192.168.20.71" # test
+        url = f"{protocol}://{host}:80/api/v1/auth/users/reset_password_confirm/"
+        print(url)
+        response = requests.post(url, data=payload)
+
+        if response.status_code == 400:
+            data = json.loads(response.content)
+            return render(request, "reset_password.html", {
+                "error": prepare_errors(data)
+            })
+
+        return render(request, "reset_password.html", {
+            "success": "Password restored successfully."
+        })
